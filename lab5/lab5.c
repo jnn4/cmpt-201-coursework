784ecb2a8bcedf572859737c7be002905bd6114b
jnn4 <jha391@sfu.ca>
2025-10-12 23:32:48 -0700

    lab 5 submission

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -16,19 +16,48 @@ void initialize_block(struct header *block, uint64_t size, struct header *next,
 }
 
 int find_first_fit(struct header *free_list_ptr, uint64_t size) {
-  // TODO: Implement first fit
+  struct header *curr = free_list_ptr;
+
+  while (curr != NULL) {
+    if (curr->size >= size) {
+      return curr->id;
+    }
+    curr = curr->next;
+  }
   return -1;
 }
 
 int find_best_fit(struct header *free_list_ptr, uint64_t size) {
   int best_fit_id = -1;
-  // TODO: Implement best fit
+  struct header *curr = free_list_ptr;
+  uint64_t smallest_diff = UINT64_MAX;
+
+  while (curr != NULL) {
+    if (curr->size >= size) {
+      uint64_t diff = curr->size - size;
+
+      if (diff < smallest_diff) {
+        smallest_diff = diff;
+        best_fit_id = curr->id;
+      }
+    }
+    curr = curr->next;
+  }
   return best_fit_id;
 }
 
 int find_worst_fit(struct header *free_list_ptr, uint64_t size) {
   int worst_fit_id = -1;
-  // TODO: Implement worst fit
+  struct header *curr = free_list_ptr;
+  uint64_t largest_size = 0;
+
+  while (curr != NULL) {
+    if (curr->size >= size && curr->size > largest_size) {
+      largest_size = curr->size;
+      worst_fit_id = curr->id;
+    }
+    curr = curr->next;
+  }
   return worst_fit_id;
 }
 
@@ -52,7 +81,30 @@ int main(void) {
   int best_fit_id = find_best_fit(free_list_ptr, 7);
   int worst_fit_id = find_worst_fit(free_list_ptr, 7);
 
-  // TODO: Print out the IDs
+  printf("The ID for First-Fit algorithm is: %d\n", first_fit_id);
+  printf("The ID for Best-Fit algorithm is: %d\n", best_fit_id);
+  printf("The ID for Worst-Fit algorithm is: %d\n", worst_fit_id);
 
+  free(free_block1);
+  free(free_block2);
+  free(free_block3);
+  free(free_block4);
+  free(free_block5);
   return 0;
 }
+
+/*
+ * part 2: coalescing contiguous free blocks
+ *
+ * first: setting prev = null, curr = free_list_ptr
+ *
+ * second: loop through, and find where the free block should go
+ *
+ * third: insert a new_free_block between prev and curr.
+ * new_free_block->curr
+ *
+ * fourth: if the blocks touch with the previous block, then merge them together
+ *
+ * fifth: if the next block touch, then merge with it as well
+ *
+ * */
